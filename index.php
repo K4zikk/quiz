cb97bc02725ac2f2e64f931cfa3e11cab25d8a58
K4zikk <113504844+K4zikk@users.noreply.github.com>
2022-10-19 09:03:08 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -11,8 +11,14 @@
     </header>
 <?php
    $con = new mysqli("127.0.0.1", "root", "", "mydb");
-    $res = $con ->query("SELECT id, question FROM questions");
-    $rows = $res -> fetch_array();
+    $res = $con ->query("SELECT * FROM questions WHERE id=1");
+    $rows = $res -> fetch_all(MYSQLI_ASSOC);
+   for($i=0;$i<count($rows);$i++)
+       echo $rows[$i]["question"]."<br>";
+    $res = $con ->query("SELECT * FROM answers WHERE questions_id=1");
+      $rows = $res -> fetch_all(MYSQLI_ASSOC);
+       for($i=0;$i<count($rows);$i++)
+       echo $rows[$i]["answer"]."<br>";
       ?>
 
 
